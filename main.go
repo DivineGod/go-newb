3e2067bb935ecc9852257a7482f13f2c7f92a1ca
Anders Rasmussen <divinegod@gmail.com>
2015-03-03 19:11:33 +1100

    ioutil and http to read from a webservice

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import (
     "fmt"
+    "net/http"
+    "io/ioutil"
 )
 
+// http://api.openweathermap.org/data/2.5/weather?q=Melbourne,au
+
 func main() {
-    fmt.Printf("Hello, World!\n")
+    resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=Melbourne,au")
+    if err != nil {
+        fmt.Printf("Error getting weather %v", err)
+        return
+    }
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Printf("Error reading weather $v", err)
+        return
+    }
+    defer resp.Body.Close()
+    fmt.Printf("Response: %s", body)
 }
 
